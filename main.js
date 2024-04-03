4ef7522a93dcdd3ba942b831181d1d91af78bbe0
laura-benavente <laurabenaventealmazan@gmail.com>
2024-04-03 18:31:24 +0200

    ejerciciomdulo3

---
+++ main.js
@@ -0,0 +1,52 @@
+
+let artist;
+// let artist: string;
+let year;
+let active;
+let genre;
+let mensaje;
+
+ artist = "The Beatles" ;
+ year = 1960 ;
+ active = true ;
+ genre = "Pop Rock" ;
+ mensaje =
+`${artist} año: ${year}, activo: ${active} genero: ${genre}.`
+
+console.log(mensaje);
+
+ artist = "Queen" ;
+ year = 1970 ;
+ active = false ;
+ genre = "Rock" ;
+ mensaje =
+`${artist} año: ${year}, activo: ${active} genero: ${genre}.`
+
+console.log(mensaje);
+
+ artist = "AC DC" ;
+ year = "1973" ;
+ active = true ;
+ genre = "Hard Rock" ;
+ mensaje =
+`${artist} año: ${year}, activo: ${active} genero: ${genre}.`
+
+console.log(mensaje);
+
+ artist = "Ludwig van Beethoven" ;
+ year = 1770 ;
+ active = false ;
+ genre = "Classic" ;
+ mensaje =
+`${artist} año: ${year}, activo: ${active} genero: ${genre}.`
+
+ console.log(mensaje);
+
+  artist = "The Rolling Stones" ;
+  year = 1962 ;
+  active = true ;
+  genre = "Rock" ;
+  mensaje =
+ `${artist} año: ${year}, activo: ${active} genero: ${genre}.`
+
+  console.log(mensaje);
